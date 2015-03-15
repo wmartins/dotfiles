c568800a6b6c4267fca7b9dff2c2598d2cd5956a
William Martins <wwwhmartins@gmail.com>
2015-03-15 12:56:45 -0300

    Add zsh config files

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 DIR=~/dotfiles
 
+install_zsh() {
+    rm ~/.zsh
+    ln -fs $DIR/zsh/zshrc ~/.zshrc
+}
+
 install_vim() {
     rm ~/.vim
     ln -fs $DIR/vim ~/.vim
@@ -10,6 +15,7 @@ install_vim() {
 
 install() {
     install_vim
+    install_zsh
 }
 
 install
