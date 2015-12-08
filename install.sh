b21695d5cc3a3040d3a97c51d3b5487ab54c9cd6
William Martins <wwwhmartins@gmail.com>
2015-12-08 00:28:57 -0200

    Add base16 colorscheme

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ install_zsh() {
     ln -fs $DIR/zsh/zshrc ~/.zshrc
 }
 
+install_base16_shell() {
+    rm -rf ~/.config/base16-shell
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+}
+
 install_vim() {
     rm ~/.vim -f
     ln -fs $DIR/vim ~/.vim
@@ -13,6 +18,7 @@ install_vim() {
 }
 
 install() {
+    install_base16_shell
     install_vim
     install_zsh
 }
