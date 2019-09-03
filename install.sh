25c8a8c6fcc7e61ee0374aec3695ac48f3296ecb
William Martins <wwwhmartins@gmail.com>
2019-09-03 01:43:57 -0300

    adds tmux configs

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 DIR=~/dotfiles
 
-install_zsh() {
-  ln -fs ${DIR}/zsh/zshrc ~/.zshrc
-}
-
 install_base16_shell() {
   rm -rf ~/.config/base16-shell
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+}
+
+install_tmux() {
+  ln -fs ${DIR}/tmux.conf ~/.tmux.conf
 }
 
 install_vim() {
@@ -21,11 +21,19 @@ install_vim_plugins() {
   vim +PluginInstall +qall
 }
 
+install_zsh() {
+  ln -fs ${DIR}/zsh/zshrc ~/.zshrc
+}
+
 install() {
   install_base16_shell
+
   install_vim
-  install_zsh
   install_vim_plugins
+
+  install_tmux
+
+  install_zsh
 }
 
 install
